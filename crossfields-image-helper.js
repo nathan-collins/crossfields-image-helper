644c2cfe555fc20a7b90eaa1502a323377ac8e04
Nathan Collins <nathanc@crossfields.com.au>
2016-12-06 09:28:38 +1000

    Tests passing and demo visable

---
+++ crossfields-image-helper.js
@@ -0,0 +1,55 @@
+Polymer({
+
+  is: 'crossfields-image-helper',
+
+  properties: {
+    icon: {
+      type: String,
+      observer: '_handleIcon'
+    },
+
+    totalHelpers: {
+      type: Number
+    },
+
+    label: {
+      type: String
+    },
+
+    imagePath: {
+      type: String
+    },
+
+    isIcon: {
+      type: Boolean,
+      value: false
+    },
+    isImage: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  _toArray: function(totalHelpers) {
+    var helpers = [];
+    if(totalHelpers > 0) {
+      for(var  i = 0; i < totalHelpers; i++) {
+        helpers.push(i);
+      }
+    }
+    return helpers;
+  },
+
+  _computeHelperLabel: function(label, totalHelpers) {
+    return pluralize(label, totalHelpers, true);
+  },
+
+  _handleIcon: function(icon) {
+    var colonExists = icon.indexOf(":");
+    if(colonExists > -1) {
+      return false;
+    }
+    return true;
+  }
+
+});
